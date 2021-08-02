1d46aafcd480e30d193f2890dd27de4c99844852
WINDOWS-2BPNGNL\EDZ <957530832@qq.com>
2021-08-02 17:10:55 +0800

    样式功能完善

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 var x=  document.getElementById("input")//获取输入框元素
 const data = [
     {
-        id: 1,
-        text: "这是一个实验",
+        text: "这是一个实验1",
         selected: false
-    }
+    },
+    {
+        text: "这是一个实验2",
+        selected: false
+    },
+    {
+        text: "这是一个实验3",
+        selected: false
+    },
 ];
 
 var listWarper = document.getElementById("listWarper");
 
-data.forEach(function(item, index) {
-    text.index = item.text;
-    click.value = item.selected;
+data.forEach(function(innerItem, index) {
+    var item = document.createElement("div");
+        item.className = "item";
+        var itemleft = document.createElement("div");
+        itemleft.className = "itemleft";
+        var click = document.createElement("div");
+        click.className = "click";
+        var text = document.createElement("div");
+        text.className = "text";
+        var delet = document.createElement("div");
+        delet.className = "delete";
+        delet.innerHTML = "删除";
+        delet.onclick = function() {
+            document.getElementById("listWarper").removeChild(item);
+        }
+        item.appendChild(itemleft);
+        item.appendChild(delet);
+        itemleft.appendChild(click);
+        itemleft.appendChild(text);
+        click.value = false;
+        text.innerText = innerItem.text;
+        document.getElementById("listWarper").appendChild(item);
 })
 
 //提交
@@ -39,7 +65,6 @@ function submit() {
         click.value = false;
         text.innerText = x.value;
         document.getElementById("listWarper").appendChild(item);
-        console.log(listWarper)
     } else {
         alert('请输入内容')
     }

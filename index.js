077ee8622955e1f387c45cf4cb60c53001c0be0c
WINDOWS-2BPNGNL\EDZ <957530832@qq.com>
2021-08-02 17:51:25 +0800

    完成队列

--- index.js
+++ index.js
@@ -13,10 +13,63 @@ const data = [
         selected: false
     },
 ];
+const finishData = [
+    {
+        text: "完成1",
+        selected: false
+    },
+    {
+        text: "完成2",
+        selected: false
+    },
+    {
+        text: "完成3",
+        selected: false
+    },
+];
 
 var listWarper = document.getElementById("listWarper");
 
+
 data.forEach(function(innerItem) {
+    var item = document.createElement("div");
+        item.className = "item";
+        var itemleft = document.createElement("div");
+        itemleft.className = "itemleft";
+        var click = document.createElement("div");
+        click.className = "click";
+        var text = document.createElement("div");
+        text.className = "text";
+        var opreat = document.createElement("div");
+        opreat.className = "opreat";
+        var finish = document.createElement("div");
+        finish.className = "finish";
+        finish.innerHTML = "完成";
+        finish.onclick = function() {
+            delet.onclick = function() {
+                document.getElementById("finish").removeChild(item);
+            }
+            opreat.removeChild(finish);
+            document.getElementById("finish").appendChild(item)
+        } 
+        var delet = document.createElement("div");
+        delet.className = "delete";
+        delet.innerHTML = "删除";
+        delet.onclick = function() {
+            document.getElementById("listWarper").removeChild(item);
+        }
+
+        item.appendChild(itemleft);
+        item.appendChild(opreat);
+        opreat.appendChild(finish);
+        opreat.appendChild(delet);
+        itemleft.appendChild(click);
+        itemleft.appendChild(text);
+        click.value = false;
+        text.innerText = innerItem.text;
+        document.getElementById("listWarper").appendChild(item);
+});
+finishData.forEach(innerItem => {
     var item = document.createElement("div");
         item.className = "item";
         var itemleft = document.createElement("div");
@@ -29,15 +82,16 @@ data.forEach(function(innerItem) {
         delet.className = "delete";
         delet.innerHTML = "删除";
         delet.onclick = function() {
-            document.getElementById("listWarper").removeChild(item);
+            document.getElementById("finish").removeChild(item);
         }
+       
         item.appendChild(itemleft);
         item.appendChild(delet);
         itemleft.appendChild(click);
         itemleft.appendChild(text);
         click.value = false;
         text.innerText = innerItem.text;
-        document.getElementById("listWarper").appendChild(item);
+        document.getElementById("finish").appendChild(item);
 })
 
 //提交
@@ -57,8 +111,22 @@ function submit() {
         delet.onclick = function() {
             document.getElementById("listWarper").removeChild(item);
         }
+        var opreat = document.createElement("div");
+        opreat.className = "opreat";
+        var finish = document.createElement("div");
+        finish.className = "finish";
+        finish.innerHTML = "完成";
+        finish.onclick = function() {
+            opreat.removeChild(finish);
+            delet.onclick = function() {
+                document.getElementById("finish").removeChild(item);
+            }
+            document.getElementById("finish").appendChild(item);
+        }
         item.appendChild(itemleft);
-        item.appendChild(delet);
+        item.appendChild(opreat);
+        opreat.appendChild(finish);
+        opreat.appendChild(delet);
         itemleft.appendChild(click);
         itemleft.appendChild(text);
     
